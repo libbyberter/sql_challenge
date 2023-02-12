9f4193ff7dedd77d062aa73f472c12577a197bab
libbyberter@gmail.com <118197596+libbyberter@users.noreply.github.com>
2023-02-12 14:38:18 -0500

    20230212 1438

---
+++ EmployeeSQL/employee_tables.sql
@@ -0,0 +1,59 @@
+﻿CREATE TABLE departments (
+    dept_no varchar   NOT NULL,
+    dept_name varchar   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE titles (
+    title_id varchar   NOT NULL,
+    title varchar   NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
+);
+
+CREATE TABLE employees (
+    emp_no int   NOT NULL,
+    emp_title_id varchar   NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+    birth_date date  NOT NULL,
+    first_name varchar   NOT NULL,
+    last_name varchar   NOT NULL,
+    sex varchar   NOT NULL,
+    hire_date date   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE dept_emp (
+    emp_no int   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    dept_no varchar   NOT NULL,
+    foreign key (dept_no) references departments (dept_no),
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        emp_no, dept_no
+     )
+);
+
+CREATE TABLE dept_manager (
+    dept_no varchar   NOT NULL,
+    foreign key (dept_no) references departments (dept_no),
+    emp_no int   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        dept_no, emp_no
+     )
+);
+
+CREATE TABLE salaries (
+    emp_no int   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    salary int not null,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no, salary
+     )
+);
+
---
+++ EmployeeSQL/postgres_query.sql
@@ -0,0 +1,78 @@
+-- create tables & import data
+
+CREATE TABLE departments (
+    dept_no varchar   NOT NULL,
+    dept_name varchar   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+select * from departments;
+---
+
+CREATE TABLE titles (
+    title_id varchar   NOT NULL,
+    title varchar   NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
+);
+
+select * from titles;
+---
+
+CREATE TABLE employees (
+    emp_no int   NOT NULL,
+    emp_title_id varchar   NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+    birth_date date  NOT NULL,
+    first_name varchar   NOT NULL,
+    last_name varchar   NOT NULL,
+    sex varchar   NOT NULL,
+    hire_date date   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+select * from employees;
+---
+
+CREATE TABLE dept_emp (
+    emp_no int   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    dept_no varchar   NOT NULL,
+    foreign key (dept_no) references departments (dept_no),
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        emp_no, dept_no
+     )
+);
+
+select * from dept_emp;
+---
+
+CREATE TABLE dept_manager (
+    dept_no varchar   NOT NULL,
+    foreign key (dept_no) references departments (dept_no),
+    emp_no int   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        dept_no, emp_no
+     )
+);
+
+select * from dept_manager;
+---
+
+CREATE TABLE salaries (
+    emp_no int   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    salary int not null,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no, salary
+     )
+);
+
+select * from salaries;
+---
